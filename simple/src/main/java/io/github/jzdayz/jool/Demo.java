8a06bec5d14a6c5d8864b72bfe0df15b2e9e9311
jzdayz <1397649957@qq.com>
2020-04-30 10:38:54 +0800

    init

---
+++ simple/src/main/java/io/github/jzdayz/jool/Demo.java
@@ -0,0 +1,16 @@
+package io.github.jzdayz.jool;
+
+import org.jooq.lambda.Unchecked;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Demo {
+    public static void main(String[] args) {
+        File dir = new File(System.getProperty("user.dir"));
+        Arrays.stream(Objects.requireNonNull(dir.listFiles()))
+                .map(Unchecked.function(File::getCanonicalPath))
+                .forEach(System.out::println);
+    }
+}
