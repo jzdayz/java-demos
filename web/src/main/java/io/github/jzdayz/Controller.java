99eadff8d7b8f9f935e4dca5e5aa50a6d766e660
jzdayz <1397649957@qq.com>
2020-05-05 20:51:53 +0800

    'x'

---
+++ web/src/main/java/io/github/jzdayz/Controller.java
@@ -0,0 +1,18 @@
+package io.github.jzdayz;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("测试类")
+@RestController
+public class Controller {
+
+    @ApiOperation("这个是个简答的测试")
+    @GetMapping
+    public Object test(String name){
+        return name;
+    }
+
+}
