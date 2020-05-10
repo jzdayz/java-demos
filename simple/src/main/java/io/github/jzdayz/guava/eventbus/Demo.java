c56b546b073e61ac821a3b559b0d8e253ccbcc68
jzdayz <1397649957@qq.com>
2020-05-10 15:45:40 +0800

    cn

---
+++ simple/src/main/java/io/github/jzdayz/guava/eventbus/Demo.java
@@ -0,0 +1,48 @@
+package io.github.jzdayz.guava.eventbus;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
+import java.util.concurrent.*;
+
+public class Demo {
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = new ThreadPoolExecutor(
+                1,1,10L,TimeUnit.SECONDS,new ArrayBlockingQueue<>(100),
+                (r)->{
+                    Thread t = new Thread(r);
+                    t.setName("哈哈家具");
+                    t.setDaemon(true);
+                    return t;
+                }
+        );
+        AsyncEventBus eventBus = new AsyncEventBus(executorService);
+
+        eventBus.register(new E());
+
+        eventBus.post(new StartedEvent());
+
+        System.out.println("done");
+
+        TimeUnit.SECONDS.sleep(2L);
+        
+    }
+
+    public static class StartedEvent{
+        @Override
+        public String toString() {
+            return "StartedEvent{}";
+        }
+    }
+
+    public static class E{
+
+        @Subscribe
+        public void listener(StartedEvent event){
+            System.out.println(Thread.currentThread());
+            System.out.println(event);
+        }
+
+    }
+}
