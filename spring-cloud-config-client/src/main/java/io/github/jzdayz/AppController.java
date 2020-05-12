4ecb408a820920301092cad376e92b2283b0df8b
胡晴风 <1397649957@qq.com>
2020-05-12 20:54:01 +0800

    cn

---
+++ spring-cloud-config-client/src/main/java/io/github/jzdayz/AppController.java
@@ -0,0 +1,32 @@
+package io.github.jzdayz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// This will allow us to reinitialize this controller to get any new config
+// values when the /refresh endpoint is POSTed to.
+@RefreshScope
+public class AppController {
+
+    @Value("${info.foo}")
+    private String fooProperty;
+
+    @Autowired
+    private ContextRefresher re;
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Using [" + fooProperty + "] from config server";
+    }
+
+//    @RequestMapping("/refresh")
+//    public String hello1() {
+//        re.refresh();
+//        return "1";
+//    }
+}
---
+++ spring-cloud-config-client/src/main/java/io/github/jzdayz/ConfigClientApp.java
@@ -0,0 +1,11 @@
+package io.github.jzdayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApp.class);
+    }
+}
