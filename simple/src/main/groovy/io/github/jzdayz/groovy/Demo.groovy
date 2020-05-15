dd6cef23a1dfaaf0bb15db8e13960c5f5c64bae2
胡晴风 <1397649957@qq.com>
2020-05-15 20:21:07 +0800

    cn

--- simple/src/main/groovy/io/github/jzdayz/groovy/Demo.groovy
+++ simple/src/main/groovy/io/github/jzdayz/groovy/Demo.groovy
@@ -1,7 +1,16 @@
 package io.github.jzdayz.groovy
 
+import org.springframework.beans.factory.BeanFactory
+
 class Demo{
+
+    private BeanFactory beanFactory;
+
+    Demo(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory
+    }
+
     static void pi(def str){
-        println("1")
+        
     }
 }
---
+++ simple/src/main/java/io/github/jzdayz/netflix/conductor/SampleWorker.java
@@ -0,0 +1,38 @@
+package io.github.jzdayz.netflix.conductor;
+
+import com.alibaba.fastjson.JSON;
+import com.netflix.conductor.client.worker.Worker;
+import com.netflix.conductor.common.metadata.tasks.Task;
+import com.netflix.conductor.common.metadata.tasks.TaskResult;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SampleWorker implements Worker {
+
+	private String taskDefName;
+
+	public SampleWorker(String taskDefName) {
+		this.taskDefName = taskDefName;
+	}
+
+	@Override
+	public String getTaskDefName() {
+		return taskDefName;
+	}
+
+	@Override
+	public TaskResult execute(Task task) {
+		TaskResult result = new TaskResult(task);
+		result.setStatus(TaskResult.Status.COMPLETED);
+		log.info("执行任务->"+ JSON.toJSONString(task));
+
+		if (taskDefName.equals("task1")){
+			result.getOutputData().put("tt","arg");
+		}else{
+			System.err.println(result.getOutputData().get("tt"));
+		}
+
+		//Register the output of the task
+		return result;
+	}
+}
