f5d1153343a220f34c97862cbdc58d7717a4d255
胡晴风 <1397649957@qq.com>
2020-05-26 18:28:59 +0800

    整理配置文件

---
+++ simple/src/main/java/io/github/jzdayz/minio/Demo.java
@@ -0,0 +1,28 @@
+package io.github.jzdayz.minio;
+
+import io.minio.ErrorCode;
+import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+
+public class Demo {
+    public static void main(String[] args) throws Exception{
+        MinioClient minioClient = new MinioClient("http://localhost:9000","123123123","123123123");
+        boolean found = false;
+        try {
+            minioClient.statObject("test", "b.zip");
+            found = true;
+        } catch (ErrorResponseException e) {
+            ErrorCode code = e.errorResponse().errorCode();
+            if (code == ErrorCode.NO_SUCH_KEY || code == ErrorCode.NO_SUCH_OBJECT) {
+            } else {
+                throw e;
+            }
+        }
+        System.out.println(found);
+        if (found) {
+
+        } else {
+
+        }
+    }
+}
