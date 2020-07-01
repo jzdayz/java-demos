e2eb453fbf3cb8c6d580faa6585c997385ded76a
huqingfeng <1397649957@qq.com>
2020-07-01 15:42:09 +0800

    some test

--- web/src/main/java/io/github/jzdayz/Controller.java
+++ web/src/main/java/io/github/jzdayz/Controller.java
@@ -2,17 +2,100 @@ package io.github.jzdayz;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 @Api("测试类")
 @RestController
 public class Controller {
 
-  @ApiOperation("这个是个简答的测试")
-  @GetMapping
-  public Object test(String name) {
-    return name;
-  }
+    @ApiOperation("这个是个简答的测试")
+    @GetMapping
+    public Object test(String name) {
+        return name;
+    }
+
+    /**
+     *  下载文件
+     * @return
+     */
+    @GetMapping("/test1")
+    public ResponseEntity<StreamingResponseBody> test1() {
+        ClassPathResource res = new ClassPathResource("/application.yaml");
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=application.yaml");
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/octet-stream");
+        return new ResponseEntity<>(
+                (output) -> {
+                    try (
+                            InputStream inputStream = res.getInputStream();
+                            ){
+                        StreamUtils.copy(inputStream, output);
+                    }
+                }, headers, HttpStatus.OK);
+    }
+
+    /**
+     *  操作zip文件的内容
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/test2")
+    @ResponseBody
+    public ResponseEntity<StreamingResponseBody> test2(MultipartFile file) throws IOException {
+        byte[] bytes = file.getBytes();
+        ZipInputStream zipInputStream = new ZipInputStream(
+                new ByteArrayInputStream(bytes), StandardCharsets.UTF_8
+        );
+        ZipEntry entry;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ZipOutputStream out = new ZipOutputStream(byteArrayOutputStream);
+        while ((entry = zipInputStream.getNextEntry()) != null){
+            System.out.println("fileName:" + entry.getName());
+            String data = StreamUtils.copyToString(zipInputStream, Charset.defaultCharset());
+
+            System.out.println(data);
+            out.putNextEntry(new ZipEntry(entry.getName()));
+            StreamUtils.copy((data+"UPDATE").getBytes(),out);
+        }
+        out.flush();
+        out.close();
+
+
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=ab.zip");
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/octet-stream");
+        return new ResponseEntity<>(
+                (output) -> {
+                    try{
+                        StreamUtils.copy(byteArrayOutputStream.toByteArray(), output);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }, headers, HttpStatus.OK);
+
+    }
 
 }
