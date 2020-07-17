30d2451fe6dcb842953830c3abfb754d9eaba0ab
huqingfeng <1397649957@qq.com>
2020-07-17 09:32:18 +0800

    some test

--- web/src/main/java/io/github/jzdayz/App.java
+++ web/src/main/java/io/github/jzdayz/App.java
@@ -1,14 +1,11 @@
 package io.github.jzdayz;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication springApplication = new SpringApplication(App.class);
-    springApplication.setWebApplicationType(WebApplicationType.NONE);
-    springApplication.run(args);
+    SpringApplication.run(App.class,args);
   }
 }
