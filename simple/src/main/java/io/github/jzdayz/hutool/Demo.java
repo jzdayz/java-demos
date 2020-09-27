9e95fba1e75acdfe9bc2c71f626c2a02e1254b89
huqingfeng <1397649957@qq.com>
2020-09-27 10:59:04 +0800

    调整

--- simple/src/main/java/io/github/jzdayz/hutool/Demo.java
+++ simple/src/main/java/io/github/jzdayz/hutool/Demo.java
@@ -8,7 +8,7 @@ import java.nio.charset.StandardCharsets;
 public class Demo {
 
     public static void main(String[] args) {
-        DESede desede = SecureUtil.desede("8989XX89988989898989xXXX".getBytes(StandardCharsets.UTF_8));
+        DESede desede = SecureUtil.desede("".getBytes(StandardCharsets.UTF_8));
         final String m = "PFl7yhUlgkt415DcZ44R8pkCP+Fm22KulVZoJDLhZH47FNnu1gi8rVLxu+RMsEtE";
         String s = desede.decryptStr(m);
         System.out.println("result:" + s);
