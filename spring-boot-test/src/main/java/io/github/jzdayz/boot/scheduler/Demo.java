cab2b2caf59a93c38bac955487b876d7e9f9acc0
huqingfeng <1397649957@qq.com>
2020-10-09 17:44:37 +0800

    CN

---
+++ spring-boot-test/src/main/java/io/github/jzdayz/boot/scheduler/Demo.java
@@ -0,0 +1,19 @@
+package io.github.jzdayz.boot.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@EnableScheduling
+@SpringBootApplication
+public class Demo {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo.class);
+    }
+
+    @Scheduled(cron = "* * * * * ?")
+    public void test(){
+        System.out.println(1);
+    }
+}
