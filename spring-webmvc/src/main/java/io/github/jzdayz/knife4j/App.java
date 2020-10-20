0871bca3b2f5f9955af2b1f2313ed78c2a2dae68
huqingfeng <1397649957@qq.com>
2020-10-20 14:25:46 +0800

    CN

---
+++ spring-webmvc/src/main/java/io/github/jzdayz/knife4j/App.java
@@ -0,0 +1,15 @@
+package io.github.jzdayz.knife4j;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableKnife4j
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
