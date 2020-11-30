0c8a8ab845cbb5c412c4de82a313f1c38ee7d4a7
huqingfeng <1397649957@qq.com>
2020-11-30 14:11:23 +0800

    X

--- simple/src/main/java/io/github/jzdayz/jdk/forkjoinpool/Demo.java
+++ simple/src/main/java/io/github/jzdayz/jdk/forkjoinpool/Demo.java
@@ -1,16 +1,33 @@
 package io.github.jzdayz.jdk.forkjoinpool;
 
+import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.concurrent.ForkJoinPool.defaultForkJoinWorkerThreadFactory;
-
 public class Demo {
     public static void main(String[] args) {
-        ForkJoinPool forkJoinPool = new ForkJoinPool(64);
-        for (int i = 0; i < 100; i++) {
+
+        final Scanner scanner = new Scanner(System.in);
+        new Thread(scanner::next,"cc").start();
+        try {
+            TimeUnit.DAYS.sleep(1L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void test1() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(20);
+        final Scanner scanner = new Scanner(System.in);
+        scanner.next();
+        for (int i = 0; i < 10000; i++) {
             forkJoinPool.execute(() -> {
                 System.out.println(Thread.currentThread().getName());
+                try {
+                    TimeUnit.SECONDS.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             });
         }
         try {
